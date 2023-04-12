90ba4319423f549ba80e0fad9eeb9476f4bb8a37
Francesco <hifrancesco@outlook.com>
2023-04-12 23:07:04 +0100

    workflow #14

--- main.py
+++ main.py
@@ -1,1 +1,51 @@
-print("Hello World")
+import os
+import time
+
+
+events = [
+    "branch_protection_rule",
+    "check_run",
+    "check_suite",
+    "create",
+    "delete",
+    "deployment",
+    "deployment_status",
+    "discussion",
+    "discussion_comment",
+    "fork",
+    "gollum",
+    "issue_comment",
+    "issues",
+    "label",
+    "merge_group",
+    "milestone",
+    "page_build",
+    "project",
+    "project_card",
+    "project_column",
+    "public",
+    "pull_request",
+    "pull_request_comment",
+    "pull_request_review",
+    "pull_request_review_comment",
+    "pull_request_target",
+    "push",
+    "registry_package",
+    "release",
+    "repository_dispatch",
+    "schedule",
+    "status",
+    "watch",
+    "workflow_call",
+    "workflow_dispatch",
+    "workflow_run",
+]
+
+def main(events):
+    os.system("clear")
+    for i, event in enumerate(events, start=1):
+        time.sleep(0.1)
+        print(f"{i}. {event}")
+
+if __name__ == "__main__":
+    main(events)
