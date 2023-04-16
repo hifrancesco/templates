b6e7c98965e8ab3fcb52dbde7bd8e627b1b0677f
Francesco <hifrancesco@outlook.com>
2023-04-16 16:38:55 +0100

    update

--- main.py
+++ main.py
@@ -41,11 +41,32 @@ events = [
     "workflow_run",
 ]
 
-def main(events):
-    os.system("clear")
+keywords = [
+    "name: specifies the name of the workflow.",
+    "on: specifies the events that trigger the workflow.",
+    "jobs: specifies one or more jobs to run as part of the workflow.",
+    "steps: specifies a sequence of steps to run as part of a job.",
+    "uses: specifies an action or a composite run command to use in the job.",
+    "env: specifies environment variables to set for the workflow.",
+    "with: provides input parameters for an action or a composite run command.",
+    "runs-on: specifies the type of machine or virtual environment to run the job on.",
+    "needs: specifies dependencies between jobs in the workflow.",
+]
+
+
+def gh_events(events):
     for i, event in enumerate(events, start=1):
         time.sleep(0.1)
         print(f"{i}. {event}")
 
+
+def gh_keywords(keywords):
+    print("\nname on jobs, steps uses env with runs-on needs\n")
+    for i, keyword in enumerate(keywords, start=1):
+        time.sleep(0.1)
+        print(f"{i}. {keyword}")
+
 if __name__ == "__main__":
-    main(events)
+    os.system("clear")
+    gh_events(events)
+    gh_keywords(keywords)
